376e475d79ba25ce0c33d32222cdfc0c8dce6e27
Fabian Vergara <vergarf@gmail.com>
2016-10-21 22:26:38 -0400

    cam view

--- Deep Sense/ViewController.swift
+++ Deep Sense/ViewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate {
 
+    @IBOutlet var cameraView: UIView!
+    
+    var captureSession: AVCaptureSession?
+    var stillImageOutput   : AVCaptureStillImageOutput?
+    var previewLayer  : AVCaptureVideoPreviewLayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,48 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer?.frame = cameraView.bounds
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        captureSession = AVCaptureSession()
+        captureSession?.sessionPreset = AVCaptureSessionPreset1920x1080
+        
+        var backCamera = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        // var error : NSError?
+        var input: AVCaptureDeviceInput?
+        //   input = AVCaptureDeviceInput(device: backCamera) throws ->
+        
+        do {
+            input = try AVCaptureDeviceInput(device: backCamera)
+            captureSession?.addInput(input)
+            
+            stillImageOutput = AVCaptureStillImageOutput()
+            stillImageOutput?.outputSettings = [AVVideoCodecKey : AVVideoCodecJPEG]
+            
+            if (captureSession?.canAddOutput(stillImageOutput) != nil ){
+                captureSession?.addOutput(stillImageOutput)
+                
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                previewLayer?.videoGravity = AVLayerVideoGravityResizeAspect
+                previewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.portrait
+                cameraView.layer.addSublayer(previewLayer!)
+                captureSession?.startRunning()
+            }
+            
+            //  var input = AVCaptureDeviceInput(device: backCamera)
+            
+        } catch let error {
+            print(error)
+        }
+        
+    }
 
 }
 
